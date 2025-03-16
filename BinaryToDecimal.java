f26d16f607f7c0cb7e0147037913b16bc224d072
Student App <tejaswinirm2004@gmail.com>
2025-03-16 13:29:56 +0530

    java task3

---
+++ BinaryToDecimal.java
@@ -0,0 +1,18 @@
+
+public class BinaryToDecimal {
+ 
+  public static void main(String args[]){
+    System.out.println(Integer.toBinaryString(10));
+System.out.println(Integer.toOctalString(10));
+System.out.println(Integer.toHexString(10));
+
+ int i=0b101;
+System.out.println(i);
+System.out.println(Integer.parseInt("i",2));
+
+
+
+}
+}
+
+
