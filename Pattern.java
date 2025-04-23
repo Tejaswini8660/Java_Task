2bb7264cf10ed9a90a5491261fd9adfb03b400bf
Tejaswini8660 <tejaswinirm2004@gmail.com>
2025-04-23 16:14:28 +0530

    Add files via upload

---
+++ Pattern.java
@@ -0,0 +1,103 @@
+import java.util.*;
+public class Pattern {
+  public static void main(String args[]){
+   //Scanner sc=new Scanner(System.in);
+//int n=sc.nextInt();
+/*for(int i=0;i<n;i++){
+  for (int j=0;j<i;j++){
+    System.out.print("*");
+  }
+  System.out.println();
+}
+
+  
+  for(int i1=0;i1<n;i1++){
+    for(int j1=0;j1<n-i1;j1++){
+      System.out.print("*");
+    }
+    System.out.println();
+    */
+    /*for(int i=1;i<=n;i++){
+      for(int j=1;j<=i;j++){
+        System.out.print(j);
+      }
+      System.out.println();*/
+      /*char ch='A';
+      for(int i=0;i<n;i++){
+        for(int j=0;j<i;j++){
+          System.out.print((int)ch);
+          ch++;
+
+        }
+        System.out.println();*/
+       /* for(int i=1;i<=n;i++){
+          for(int j=1;j<=i;j++){
+            if((i+j)%2==0){
+              System.out.print(1);
+            }
+            else{
+              System.out.print(0);
+            }
+          }
+          System.out.println();*/ 
+         /* for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+              System.out.print(j);
+            }
+            System.out.println();*/
+/*int number=sc.nextInt();
+if(number<2500){
+  System.out.println((number*18/100));
+}else{
+  System.out.println((number*50/100));*/
+ 
+/* int n=sc.nextInt();
+  int tax;
+  if(n<500000){
+    tax=0;
+
+  }else if(n>=500000 && n<1000000){
+    tax=(int)(n*0.2);
+    System.out.println(n-tax);
+
+}else{
+  tax=(int)(n*0.3);
+System.out.println(n-tax);
+
+            }
+            System.out.println("Tax is: "+tax);
+          }
+        }*/
+    /*for (int i=1;i<=100;i++){
+      if(i%2==0){
+        System.out.println(i+" is even number");
+      
+      }else if(i%2==1){
+        System.out.println(i+" is odd number");
+      }*/
+     /* Scanner sc=new Scanner(System.in);
+      int n=sc.nextInt();
+int n1=sc.nextInt();
+      int n2=sc.nextInt();
+      
+int n4=n+n1+n2;
+int tax=0;
+      tax=n4+(n4*18/100);
+      System.out.println(tax);*/
+      Scanner sc=new Scanner(System.in);
+      int n=sc.nextInt();
+      String n1=((n>35)?"pass":"fail");
+System.out.println(n1);
+
+    }
+  }
+
+
+    
+    
+        
+
+
+
+  
+
